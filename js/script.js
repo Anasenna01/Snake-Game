bf01eb0f48f788b76f1799e4402979230c58048e
Ana Senna <anapaulafreitasilva201272@gmail.com>
2023-12-13 16:30:07 -0300

    script.js

--- js/script.js
+++ js/script.js
@@ -3,12 +3,29 @@ const ctx = canvas.getContext('2d')
 
 const size = 30
 
-const snake = [{x: 200, y: 200}, {x: 230, y: 200}, {x: 260, y: 200}]
+const snake = [{x: 200, y: 200}, {x: 230, y: 200}]
+
+let direction 
 
 const drawSnake = () => {
-    ctx.fillStyle = '#ddd'
+    ctx.fillStyle = '#36ff04'
 
-    snake.forEach(() => {})
+    snake.forEach((position, index) => {
+        if(index == snake.length - 1) {
+            ctx.fillStyle = "green"
+        }
+        ctx.fillRect(position.x, position.y, size, size)
+    })
+}
+
+const moveSnack = () => {
+    const head = snake[snake.length -1]
+
+    snake.shift()
+
+    if(direction == "right"){
+        snake.push({x: head.x + size, y: head.y})
+    }
 }
 
 drawSnake()
